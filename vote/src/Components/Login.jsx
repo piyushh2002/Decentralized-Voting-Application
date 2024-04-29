af102f887b791ab35092e77b834a7c0eba25f857
Piyush <145914319+piyushh2002@users.noreply.github.com>
2024-04-29 20:03:32 +0530

    Add files via upload
    Decentralized Voting Application
    
    Welcome to the Decentralized Voting Application! This project demonstrates how to implement voting using Solidity smart contracts and ReactJS. By leveraging blockchain technology, we ensure transparency, security, and immutability in the voting process.
    
    Features
    Decentralized Voting:
    Users can cast their votes securely on the Ethereum blockchain.
    Each vote is recorded as a transaction, making it tamper-proof and transparent.
    MetaMask Integration:
    We’ve integrated MetaMask for seamless user authentication.
    Users can connect their MetaMask wallet to participate in the voting process.
    Real-Time Notifications:
    Discord is used for real-time notifications and interactions.
    Users receive updates on voting results and other relevant information.
    Installation
    Follow these steps to set up the application:
    
    Clone the repository:
    download the project
    
    Install dependencies:
    npm install
    
    Compile and deploy the smart contract:
    npx hardhat compile
    npx hardhat run --network volta scripts/deploy.js
    
    Update environment variables:
    Copy the contract address and paste it in the .env file.
    Specify the blockchain endpoint (if using a different network) in hardhat-config.js.
    Start the application:
    npm start
    
    Resources
    GitHub Repository: Check out the code and contribute!
    Research Paper: Learn more about the architecture and design.
    Tutorial: Step-by-step guide on building a decentralized voting App.
    Feel free to explore, enhance, and adapt this application to your specific requirements. Happy coding! 🚀

---
+++ vote/src/Components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            <h1 className="welcome-message">Welcome to a voting application</h1>
+            <button className="login-button" onClick = {props.connectWallet}>Login</button>
+        </div>
+    )
+}
+
+export default Login;
